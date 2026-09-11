fa28e0eb9065d4353c513bd925f9a9056c781aed
Juan Francisco Fernandez Herreros <31112111+juanfranciscofernandezherreros@users.noreply.github.com>
2026-09-11 11:20:24 +0200

    Adapt state machine example to order workflow

--- src/main/java/com/example/maquinadeestados/model/FileState.java
+++ src/main/java/com/example/maquinadeestados/model/FileState.java
@@ -1,11 +1,9 @@
 package com.example.maquinadeestados.model;
 
 public enum FileState {
-    RECEIVED,
-    DATA_READY,
-    FILE_CREATED,
-    FILE_SAVED,
-    RESPONSE_SENT,
-    FINISHED,
-    ERROR
+    CREATED,
+    PAID,
+    SHIPPED,
+    CANCELLED,
+    REFUNDED
 }
