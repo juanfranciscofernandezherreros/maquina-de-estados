968460c0b9d7dea446d7d8a8da0b44f433a6179a
Juan Francisco Fernandez Herreros <31112111+juanfranciscofernandezherreros@users.noreply.github.com>
2026-09-11 09:25:50 +0200

    Add file states

---
+++ src/main/java/com/example/maquinadeestados/model/FileState.java
@@ -0,0 +1,11 @@
+package com.example.maquinadeestados.model;
+
+public enum FileState {
+    RECEIVED,
+    DATA_READY,
+    FILE_CREATED,
+    FILE_SAVED,
+    RESPONSE_SENT,
+    FINISHED,
+    ERROR
+}
