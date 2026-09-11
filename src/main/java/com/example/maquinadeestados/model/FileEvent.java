f7e3d3ea617976ee7ef69a40b9609d091d1c2f91
Juan Francisco Fernandez Herreros <31112111+juanfranciscofernandezherreros@users.noreply.github.com>
2026-09-11 11:20:30 +0200

    Adapt events to order workflow

--- src/main/java/com/example/maquinadeestados/model/FileEvent.java
+++ src/main/java/com/example/maquinadeestados/model/FileEvent.java
@@ -1,10 +1,8 @@
 package com.example.maquinadeestados.model;
 
 public enum FileEvent {
-    DATA_READY,
-    FILE_CREATED,
-    FILE_SAVED,
-    RESPONSE_SENT,
-    ACK_RECEIVED,
-    FAIL
+    PAY,
+    CANCEL,
+    SHIP,
+    REFUND
 }
